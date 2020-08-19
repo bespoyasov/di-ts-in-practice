b5abfcd0db723fb50758ec7e2ca147ef546c1b03
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-19 21:06:34 +0300

    Remove date registration, use a getter for accessing source

--- src/adapters/dateTimeSource/browserDateTimeSource.ts
+++ src/adapters/dateTimeSource/browserDateTimeSource.ts
@@ -2,7 +2,10 @@ import {DateTimeSource} from './types';
 import {UtcDateTimeString, TimeStamp} from '@domain';
 
 export class BrowserDateTimeSource implements DateTimeSource {
-  constructor(private source: Date) {}
+  get source() {
+    return new Date();
+  }
+
   public toString = (): UtcDateTimeString => this.source.toUTCString();
   public valueOf = (): TimeStamp => this.source.getTime();
 }
--- src/adapters/dateTimeSource/composition.ts
+++ src/adapters/dateTimeSource/composition.ts
@@ -2,5 +2,4 @@ import {container} from '@composition/core';
 import {DateTimeSource} from './types';
 import {BrowserDateTimeSource} from './browserDateTimeSource';
 
-container.registerTransient<Date>();
 container.registerSingleton<DateTimeSource, BrowserDateTimeSource>();
