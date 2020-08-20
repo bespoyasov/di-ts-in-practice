058369e505d66467766c6c84b7a2df8fca5048e1
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-20 13:02:34 +0300

    Remove obsolete dependency from Logger implementation

--- src/effects/logger/consoleLogger.ts
+++ src/effects/logger/consoleLogger.ts
@@ -1,6 +1,5 @@
 import {Logger, LogEntry} from './types';
 
 export class ConsoleLogger implements Logger {
-  constructor(private env: Window) {}
   public log = (message: LogEntry): void => console.log(message);
 }
